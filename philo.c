9ba9d23068384cf0c83ff47eb8301dd5c4baf31f
Ksenia Karemina <kseniakaremina@MacBook-Pro-Ksenia.local>
2024-09-11 19:50:33 +0200

    create forks

--- philo.c
+++ philo.c
@@ -37,50 +37,142 @@ void check_input(int * inp)
         exit(0);}
 
 }
-void* philo_routine(void * arg)
-{
-    t_philo *philo = arg;
 
+int lock_2_forks(t_philo *philo, size_t max_ms) {
+    size_t start = time_now();
+    size_t end = start + max_ms;
+    t_fork* left = philo->left_fork;
+    t_fork* right = philo->right_fork;
 
-    printf("ruunning philo: id=%d tte=%d ttd=%d tts=%d\n", 
-        philo->id, (int)(philo->time_to_eat),(int)(philo->time_to_die), (int)(philo->time_to_sleep));
-    return NULL;
-}
+    while(time_now() < end) {
+        pthread_mutex_lock(&left->mu);
+        pthread_mutex_lock(&right->mu);
+        if (left->locked == 0 && right->locked == 0) {
+            left->locked = 1;
+            right->locked = 1;
+            pthread_mutex_unlock(&left->mu);
+            pthread_mutex_unlock(&right->mu);
+            return 1;
+        }
+        pthread_mutex_unlock(&left->mu);
+        pthread_mutex_unlock(&right->mu);
+        usleep(10000);   
+    }
 
-size_t time_now() {
     return 0;
 }
 
-t_philo * fill_philo(int id,int * inp)
-{
-    t_philo *philo = malloc(sizeof(t_philo));
-    philo->id = id;
-    philo->meals_eaten = 0;
-    philo->start_time = time_now();
-    philo->time_to_die = inp[1];
-    philo->time_to_eat = inp[2];
-    philo->time_to_sleep = inp[3];
-    philo->num_of_philos = inp[0];
-    philo->num_times_to_eat = inp[4];
+void unlock_fork(t_fork* mu) {
+    pthread_mutex_lock(&mu->mu);
+    mu->locked = 0;
+    pthread_mutex_unlock(&mu->mu);
+}
 
-    return(philo);
+void report(int id, char* action) {
+	printf("%lu %d is %s\n", time_now(), id + 1, action);
+}
+
+void thinking(t_philo *philo) {
+    report(philo->id, "thinking");
+    size_t time_to_live = philo->time_to_die - philo->time_to_sleep;
+    if (lock_2_forks(philo, time_to_live)) {
+        return;
+    }
+    philo->dead = 1;
+}
+
+void eating (t_philo *philo)
+{
+    report(philo->id, "eating");
+    usleep(philo->time_to_eat*1000);
+    philo->meals_eaten++;
+    philo->time_since_eaten = 0;
+    unlock_fork(philo->left_fork);
+    unlock_fork(philo->right_fork);
+}
+
+void	sleeping(t_philo *philo)
+{
+    report(philo->id, "sleeping");
+    if (philo->time_to_die<= philo->time_to_sleep)
+        {
+            usleep((philo->time_to_sleep - philo->time_to_die)*1000);
+            philo->dead = 1;
+            return;
+        }
+	usleep((philo->time_to_sleep)*1000);
+    philo->time_since_eaten = philo->time_to_sleep;
+}
+void* philo_routine(void * arg)
+{   
+
+    t_philo *philo = arg;
+
+    printf("ruunning philo: id=%d tte=%d ttd=%d tts=%d\n", 
+        philo->id, (int)(philo->time_to_eat),(int)(philo->time_to_die), (int)(philo->time_to_sleep));
+
+    while ((philo->dead!=1)&&(philo->num_times_to_eat>philo->meals_eaten))
+    {
+        thinking(philo);
+        eating(philo);
+        sleeping(philo);
+    }
+    return NULL;
 }
 
 
+t_philo * fill_philo(int id,int * inp, t_fork *forks)
+{
+    t_philo *philo = malloc(sizeof(t_philo));
+    int n = inp[0];
+    philo->id = id;
+    philo->dead = 0;
+    philo->meals_eaten = 0;
+    philo->start_time = time_now();
+    philo->time_since_eaten = time_now();
+    philo->time_to_die = inp[1];
+    philo->time_to_eat = inp[2];
+    philo->time_to_sleep = inp[3];
+    philo->num_of_philos = n;
+    philo->num_times_to_eat = inp[4];
+    philo->is_alive = 1;
+    philo->right_fork = &forks[id];
+    if (id == 0)
+        philo->left_fork = &forks[n-1];
+    else 
+        philo->left_fork = &forks[id-1];
+    return(philo);
+}
 
-void create_philo(int philo_count, int *inp)
+t_fork *create_forks(int philo_count)
+{   int i = 0;
+    t_fork *forks = malloc(philo_count*sizeof(t_fork));
+    while(i < philo_count)
+    {
+        forks[i].locked = 0;
+        pthread_mutex_init(&forks[i].mu, NULL);
+        i++;
+    }
+    return(forks);
+
+
+}
+
+void create_philo(int philo_count, int *inp, t_fork *forks)
 {
     pthread_t *threads = malloc(philo_count * sizeof(pthread_t));
     int i = 0;
     t_philo * philo;
     while(i < philo_count)
     {   
-        philo = fill_philo(i, inp); // TODO free philo later
+        philo = fill_philo(i, inp, forks); // TODO free philo later
         if (pthread_create(&threads[i], NULL, philo_routine, philo) != 0) {
             printf("Failed to create thread\n");
+            exit(1);
+        }
+        printf("Thread %d has started\n", i);
+        i++;
     }
-    i++;
-    printf("Thread %d has started\n", i);}
     for (i = 0; i < philo_count; i++) {
         if (pthread_join(threads[i], NULL) != 0) {
             printf("error joining\n");
@@ -117,13 +209,24 @@ int ft_atoi(char * a )
     return(res*sign);
     }
 
+
+size_t	time_now(void)
+{
+	struct timeval	time;
+
+	if (gettimeofday(&time, NULL) == -1)
+		write(2, "gettimeofday() error\n", 22);
+	return (time.tv_sec * 1000 + time.tv_usec / 1000);
+}
+
 int main(int argc, char *argv[])
 {
     int inp[5];
     parse_input(inp,argc-1,argv);
     int philo_count= inp[0];
     check_input(inp);
-    create_philo(philo_count, inp);
+    t_fork* forks =create_forks(philo_count);
+    create_philo(philo_count, inp, forks);
     
     return (0);
 }
--- philo.h
+++ philo.h
@@ -5,24 +5,37 @@
 # include <stdlib.h>
 # include <unistd.h>
 #include <pthread.h>
+#include <sys/time.h>
 
+
+typedef struct s_fork
+{
+    pthread_mutex_t mu;
+    int locked;
+} t_fork;
 
 typedef struct s_philo
 {
     int				id;
+    int             dead;
     int				meals_eaten;
     size_t			start_time;
+    size_t          time_since_eaten;
     size_t			time_to_die;
     size_t			time_to_eat;
     size_t			time_to_sleep;
     int				num_of_philos;
     int				num_times_to_eat;
+    int             is_alive;
+    t_fork          *left_fork;
+    t_fork          *right_fork;
 }t_philo;
 
+size_t	time_now(void);
 void parse_input(int * inp, int argc, char *argv[]);
 int ft_atoi(char * a );
 void* philo_routine(void * arg);
-void create_philo(int philo_count, int *inp);
+void create_philo(int philo_count, int *inp, t_fork *forks);
 
 
 
